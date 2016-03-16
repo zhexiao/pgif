2afbc8aca1b38a7349e1f3de6f3bdfe2b93e09c1
zhexiao <zhexiao@163.com>
2016-03-16 14:39:57 -0400

    init

--- convert/urls.py
+++ convert/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     url(r'^video\/?$', ConvertVideo.as_view()),
-    url(r'^app\/?$', HomeView.as_view(), name='home'),
+    url(r'^\/?$', HomeView.as_view(), name='home'),
 ]
--- pgif/urls.py
+++ pgif/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.conf import settings
 import rest_framework_swagger
+from convert.views import *
 
 urlpatterns = [
     url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^convert/', include('convert.urls', namespace='convert', app_name='convert')),
+    url(r'^$', HomeView.as_view())
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
